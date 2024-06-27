bc5622909ddcedc7078870b78a4f4ddff92dfd45
Alex <s.bosov2012@yandex.ru>
2024-06-27 19:02:32 +0300

    feat: add forgetting fileEntity

---
+++ core/src/main/java/com/lms/core/model/entity/FileEntity.java
@@ -0,0 +1,37 @@
+package com.lms.core.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "file", schema = "lms")
+public class FileEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id")
+    private Long fileId;
+
+    @Column(name = "orig_name")
+    private String origName;
+
+    @Column(name = "file_name")
+    private String fileName;
+
+    @Column(name = "bucket")
+    private String bucket;
+}
