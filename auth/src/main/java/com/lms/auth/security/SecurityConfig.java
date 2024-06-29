2ccc950f1a9504c98b914ac9f8cee1a8c38752de
Alex <s.bosov2012@yandex.ru>
2024-06-29 19:09:14 +0300

    fix: fix CORS

--- auth/src/main/java/com/lms/auth/security/SecurityConfig.java
+++ auth/src/main/java/com/lms/auth/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.lms.auth.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,6 +15,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static com.lms.auth.model.enums.Role.ADMINISTRATOR;
 import static com.lms.auth.model.enums.Role.TEACHER;
@@ -27,41 +31,44 @@ public class SecurityConfig {
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${allowed-origin}")
+    private String allowedOrigin;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
-        //        http.cors().disable();
-        http
+        http.cors(Customizer.withDefaults())
                 .csrf().disable()
                 .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/api/auth/**").permitAll()
-                                .requestMatchers(HttpMethod.POST, "/api/categories/**").hasAuthority(ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.DELETE, "/api/categories/**").hasAuthority(ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.PATCH, "/api/categories/**").hasAuthority(ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.PUT, "/api/categories/**").hasAuthority(ADMINISTRATOR.name())
+                                authorizeRequests
+                                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                                        .requestMatchers("/api/auth/**").permitAll()
+                                        .requestMatchers(HttpMethod.POST, "/api/categories/**").hasAuthority(ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.DELETE, "/api/categories/**").hasAuthority(ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.PATCH, "/api/categories/**").hasAuthority(ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.PUT, "/api/categories/**").hasAuthority(ADMINISTRATOR.name())
 
-                                .requestMatchers(HttpMethod.POST, "/api/attachments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.DELETE, "/api/attachments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.PATCH, "/api/attachments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.PUT, "/api/attachments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.POST, "/api/attachments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.DELETE, "/api/attachments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.PATCH, "/api/attachments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.PUT, "/api/attachments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
 
-                                .requestMatchers(HttpMethod.POST, "/api/chapters/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.DELETE, "/api/chapters/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.PATCH, "/api/chapters/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.PUT, "/api/chapters/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.POST, "/api/chapters/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.DELETE, "/api/chapters/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.PATCH, "/api/chapters/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.PUT, "/api/chapters/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
 
-                                .requestMatchers(HttpMethod.POST, "/api/courses/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.DELETE, "/api/courses/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.PATCH, "/api/courses/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.PUT, "/api/courses/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.POST, "/api/courses/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.DELETE, "/api/courses/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.PATCH, "/api/courses/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.PUT, "/api/courses/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
 
 //                                TODO: TO DO
-                                .requestMatchers(HttpMethod.POST, "/api/enrollments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.DELETE, "/api/enrollments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.PATCH, "/api/enrollments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
-                                .requestMatchers(HttpMethod.PUT, "/api/enrollments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.POST, "/api/enrollments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.DELETE, "/api/enrollments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.PATCH, "/api/enrollments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
+                                        .requestMatchers(HttpMethod.PUT, "/api/enrollments/**").hasAnyAuthority(TEACHER.name(), ADMINISTRATOR.name())
 
-                                .anyRequest().authenticated()
+                                        .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults())
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
@@ -79,5 +86,17 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder(passwordEncoder);
 
         return authenticationProvider;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin(allowedOrigin);
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
